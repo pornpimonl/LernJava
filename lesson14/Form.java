0568b323eefb132b6a6cdf9fd81a6e61b9ad1d97
Joy PimOn Lakvirote <aweqaweq@hotmail.com>
2021-11-04 17:48:53 +0700

    Add public

--- lesson14/Form.java
+++ lesson14/Form.java
@@ -1,6 +1,6 @@
 package lesson14;
 
 public interface Form{
-    void apply(String name,String sername,String address,int applyid);
-    void resign(int id);
+    public void apply(String name,String sername,String address,int applyid);
+    public void resign(int id);
 }
