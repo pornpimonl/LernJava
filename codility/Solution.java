f9469b35e536fb445f18cb1803720c50f6e9b186
Joy PimOn Lakvirote <aweqaweq@hotmail.com>
2021-11-25 01:03:02 +0700

    Solution Complete

--- codility/Solution.java
+++ codility/Solution.java
@@ -1,18 +1,39 @@
 package codility;
 
-class Solution {
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println(s.solution(529));
-    }
-    public int solution(int N) {
-        // int x;
-        String s = Integer.toBinaryString(N);
-        char[] c = s.toCharArray();
-        // System.out.println(c);
-        for(int i=0;i<c.length;i++){
-                   
+import java.io.*;
+
+class Solution{
+    public int solution(int N){
+        int w=0;
+        int x=0;
+        String y = Integer.toBinaryString(N);
+        String z[]=y.split("1");
+        for(int i=0;i<y.length();i++){
+            if(Integer.parseInt(y.substring(i,i+1))==1){
+                w++;
+            }
         }
-        return c[1];
+        if(w!=1){
+            for(int j=0;j<z.length;j++){
+                if(x<z[j].length()){
+                    x=z[j].length();
+                }
+            }
+        }else{
+            x=0;
+        }
+        return x;  
     }
+    public static void main(String[] args) {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        try{
+            System.out.print("Enter Num : ");
+            int v = Integer.parseInt(in.readLine());
+            Solution b = new Solution();
+            System.out.println(b.solution(v));
+        }catch(IOException e){
+            System.out.println("Error");
+        }
+    }
+    
 }
